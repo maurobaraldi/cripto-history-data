093f88965ecbb10c5312bdb52784a78453590619
Mauro Navarro Baraldi <mauro.baraldi@farftech.com>
2022-12-17 15:58:24 +0000

    Tests

--- update.py
+++ update.py
@@ -3,15 +3,15 @@
 from pathlib import Path
 
 from git import Repo
-import pdb; pdb.set_trace()
 
 repo = Repo('./')
-diff = repo.index.diff(None, name_only=True)
+diff = repo.git.diff(None, name_only=True)
 
+import pdb; pdb.set_trace()
 if len(diff) == 0:
     exit()
 
-for f in diff:
+for f in diff.split('\n'):
     if Path(f).suffix == '.csv':
         repo.index.add(f)
         repo.index.commit(f'Update {Path(f).name} Binance')
